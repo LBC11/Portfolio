5ba24791b940c2229e47bbbd7c1404a01a219069
LBC <wpdltm4@gmail.com>
2023-04-20 21:49:24 +0900

    Configure project with Spring Boot and necessary dependencies
    The Gradle build system is used to configure the project with Groovy and Java languages. Spring Boot version 2.7.11 is specified in the build.gradle file. The necessary dependencies, including Spring Web, Spring Data JPA, MariaDB Driver, and Lombok, are added to develop a web application with Spring Framework.

---
+++ portfolio-spring/src/main/java/com/bob/portfoliospring/MyPersonalPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.bob.portfoliospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPersonalPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPersonalPortfolioApplication.class, args);
+	}
+
+}
---
+++ portfolio-spring/src/test/java/com/bob/portfoliospring/MyPersonalPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bob.portfoliospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPersonalPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
